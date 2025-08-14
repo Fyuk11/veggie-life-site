cf5f4091f2dc2aadcf88e93fbeb4e95116b3f33e
Fyuk11 <rodrigomez.11e@gmail.com>
2025-08-14 11:38:45 -0300

    Actualización de estilos

--- js/main.js
+++ js/main.js
@@ -132,3 +132,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Footer – año dinámico
 document.getElementById('year').textContent = new Date().getFullYear();
+
+// botones
+const filterButtons = document.querySelectorAll('.filter-btn');
+const menuCards = document.querySelectorAll('.menu-card');
+
+filterButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    filterButtons.forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+
+    const filter = btn.dataset.filter;
+    menuCards.forEach(card => {
+      if (filter === 'all' || card.dataset.category === filter) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  });
+});
